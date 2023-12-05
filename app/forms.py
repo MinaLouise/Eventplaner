b3261861ca21c9a1dc38e5ab91aa5b0d7d90e663
Mina <msanford24@basecampcodingacademy.org>
2023-12-05 10:22:10 -0600

    no error messages

--- app/forms.py
+++ app/forms.py
@@ -4,10 +4,14 @@ from django.utils import timezone
 from datetime import datetime
 
 class AddEventsForm(forms.Form):
-    title = forms.CharField()
-    date = forms.DateField()
-    time = forms.TimeField()
-    location = forms.CharField()
+    title = forms.CharField(required=True, error_messages={"required":""}, widget=forms.TextInput(attrs={'placeholder': 'Your event', 'required': 'true'}))
+    date = forms.DateField(required=True, error_messages={"required":""}, widget=forms.TextInput(attrs={'placeholder': 'Event date', 'required': 'true'}))
+    time = forms.TimeField(required=True, error_messages={"required":""}, widget=forms.TextInput(attrs={'placeholder': 'Event time', 'required': 'true'}))
+    location = forms.CharField(required=True, error_messages={"required":""}, widget=forms.TextInput(attrs={'placeholder': 'Event location', 'required': 'true'}))
+
+    def clean(self):
+        cleaned_data = super().clean()
+        return cleaned_data
 
 class SearchEventForm(forms.Form):
     searchtitle = forms.CharField(required=False)
--- app/static/index.js
+++ app/static/index.js
@@ -1,7 +1,24 @@
-function scrollToTop() {
-    // Scroll to the top of the page smoothly
-    window.scrollTo({
-      top: 0,
-      behavior: 'smooth'
-    });
+function validateForm() {
+  // You can replace this with the actual input fields in your form
+  var title = document.forms["form"]["title"].value;
+  var date = document.forms["form"]["date"].value;
+  var time = document.forms["form"]["time"].value;
+  var location = document.forms["form"]["location"].value;
+
+  if (title === "" || date === "" || time === "" || location === "") {
+      document.getElementById("errorMessage").innerHTML = "All feilds must be filled out";
+      document.getElementById("errorMessage").style.display = "block";
+      return false;
   }
+
+  document.getElementById("errorMessage").style.display = "none";
+  return true;
+}
+
+function scrollToTop() {
+  // Scroll to the top of the page smoothly
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
